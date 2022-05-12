8fdb59cb84b5e014a082c7d51e797a815187855c
nekopudding <61680928+nekopudding@users.noreply.github.com>
2022-05-12 11:56:02 -0700

    react app init

---
+++ src/App.js
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <div className="App">
+      {/* <!-- Sidebar start --> */}
+
+      My Documents
+
+      New Document
+
+      {/* <!-- Sidebar end -->*/}
+
+      {/*<!-- Header start --> */}
+
+      Document Name
+
+      Save Changes
+
+      {/* <!-- Header end -->*/}
+
+      {/*<!-- Main start --> */}
+
+      Markdown
+
+      Preview
+
+      {/* <!-- Main end --> */}
+    </div>
+  );
+}
+
+export default App;
