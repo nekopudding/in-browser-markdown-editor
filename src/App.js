6822ef4cf405b5876a8f810f411d52911ba419b3
nekopudding <61680928+nekopudding@users.noreply.github.com>
2022-05-22 13:09:21 -0700

    removed logging

--- src/App.js
+++ src/App.js
@@ -120,7 +120,6 @@ function App() {
   function resolveFilename(filename,id,n = 0) {
     if (fileList.filter(file => file.id !== id && file.name === filename).length !== 0) { //if there is a different file with same name
         const name = filename.split(".md")[0].split("(")[0] //remove .md and (n) suffix
-        console.log(name);
         return resolveFilename(`${name}(${n+1}).md`, id, n+1);
     }
     return filename;
